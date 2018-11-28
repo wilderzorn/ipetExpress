5cf8ef06dc959f95782b877fe6baba1e02baef0f
zhangzhihao <zhangzhihao910714@163.com>
2018-11-28 23:45:44 +0800

    门店列表的方法，暴露出去一些接口，门店分页列表

--- dao/shopDao.js
+++ dao/shopDao.js
@@ -1,1 +1,21 @@
 const mongoose = require("mongoose")
+
+module.exports.getAllShopByPage = async ({ curPage, eachPage }) => {
+  console.log(123123123);
+  
+  console.log({ curPage, eachPage });
+  let result = {
+    curPage: Number(curPage),
+    eachPage: Number(eachPage)
+  }
+  const shopModule = mongoose.model("shop");
+  result.total = await shopModule.count();
+  result.maxPage = Math.ceil(result.total / result.eachPage);
+  result.rows = await shopModule
+    .find()
+    .skip((result.curPage - 1) * result.eachPage)
+    .limit(result.eachPage)
+    .sort({ _id: 1 })
+  // .populate("classId")
+  return result;
+}
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var { getAllShopByPage } = require('../dao/shopDao.js')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+// 所有门店
+router.get('/getAllShopByPageAsync', async function (req, res, next) {
+  console.log(req.query);
+  res.send(await getAllShopByPage(req.query))
 });
 
 module.exports = router;
