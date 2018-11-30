b2d58b7ca154285b85c2ade62080677b8711207f
Lucas <1914301221@qq.com>
2018-11-30 17:55:20 +0800

    商品

--- dao/database.js
+++ dao/database.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-require("./model/user.js")
+require("./model/goods.js")
 var dbURI = 'mongodb://localhost/ipet';
 
-mongoose.connect(dbURI);
+mongoose.connect(dbURI, {
+  useNewUrlParser:
+    true
+});
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
