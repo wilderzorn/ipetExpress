e3dc7c8906ece14185221019b0c36a43fb8117e8
yanjuntao <448522158@qq.com>
2018-12-03 09:29:18 +0800

    后台修改宠物信息，删除宠物信息

--- dao/petsDao.js
+++ dao/petsDao.js
@@ -23,3 +23,26 @@ module.exports.addPet = async ({ petsName, petsSpecies, petsType, petsGender, pe
     let result = { petsName, petsSpecies, petsType, petsGender, petsPrice: Number(petsPrice), petsImg, petsColor, petsBirthday, petsCharacter }
     return await mongoose.model("pets").create(result);
 }
+
+//修改页面显示
+module.exports.updatePetById = async (_id) => {
+    return await mongoose
+        .model("pets")
+        .find(_id)
+}
+
+//修改
+module.exports.updatePet = async ({ _id, petsName, petsSpecies, petsType, petsGender, petsPrice, petsImg, petsColor, petsBirthday, petsCharacter }) => {
+    return await mongoose
+        .model("pets")
+        .update({
+            _id
+        }, {
+            $set: { petsName, petsSpecies, petsType, petsGender, petsPrice: Number(petsPrice), petsImg, petsColor, petsBirthday, petsCharacter }
+        });
+}
+
+//删除
+module.exports.removePet = async (data) => {
+    return await mongoose.model("pets").remove(data)
+}
